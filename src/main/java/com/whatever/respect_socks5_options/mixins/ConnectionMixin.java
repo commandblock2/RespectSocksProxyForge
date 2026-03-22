4f8011a1fcb778a558baca46d17af649ef900d5d
commandblock2 <tony_hu@live.com>
2026-03-22 22:09:30 +0800

    Fix SOCKS handler classloading and bundle Netty proxy libs

--- src/main/java/com/whatever/respect_socks5_options/mixins/ConnectionMixin.java
+++ src/main/java/com/whatever/respect_socks5_options/mixins/ConnectionMixin.java
@@ -1,13 +1,14 @@
 package com.whatever.respect_socks5_options.mixins;
 
 import io.netty.channel.*;
-import io.netty.handler.proxy.Socks5ProxyHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.lang.reflect.Constructor;
 import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 
 @Mixin(targets = "net.minecraft.network.Connection$1")
 public class ConnectionMixin {
@@ -20,6 +21,20 @@ public class ConnectionMixin {
         }
 
         int port = Integer.getInteger("socksProxyPort", 1080);
-        channel.pipeline().addFirst(new Socks5ProxyHandler(new InetSocketAddress(host, port)));
+        ChannelHandler proxyHandler = createProxyHandler(new InetSocketAddress(host, port));
+        if (proxyHandler != null) {
+            channel.pipeline().addFirst(proxyHandler);
+        }
+    }
+
+    private static ChannelHandler createProxyHandler(InetSocketAddress proxyAddress) {
+        try {
+            Class<?> proxyHandlerClass = Class.forName("io.netty.handler.proxy.Socks5ProxyHandler");
+            Constructor<?> constructor = proxyHandlerClass.getConstructor(SocketAddress.class);
+            Object handler = constructor.newInstance(proxyAddress);
+            return (ChannelHandler) handler;
+        } catch (Throwable ignored) {
+            return null;
+        }
     }
 }
